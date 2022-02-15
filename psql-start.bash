9303933ef5880d06e4aa92a7ba094013dfe57ae1
Alan <cloojure@gmail.com>
2022-02-15 10:40:45 -0800

    misc

--- psql-start.bash
+++ psql-start.bash
@@ -4,3 +4,4 @@
 docker run -it  --rm  --network host  --env PGPASSWORD=docker  postgres:latest  \
   psql -U postgres -h localhost  -p 5432  "$@"      # <= 2nd line invokes `psql` within the container
 
+
